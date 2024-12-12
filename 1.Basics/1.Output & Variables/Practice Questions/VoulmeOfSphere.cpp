87afbd1d0c5e6c3d608626086595b957a32200e3
Samiksha <150701933+mehtaSamiksha@users.noreply.github.com>
2024-12-12 21:52:32 +0530

    Delete 1.Basics/1.Output & Variables directory

--- 1.Basics/1.Output & Variables/Practice Questions/VoulmeOfSphere.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
-    double radius;
-    std::cin >> radius;
-    double volume = (4.0 / 3.0) * 3.14159 * radius * radius * radius;
-    std::cout << volume;
-    return 0;
-}
--- 1.Basics/1.Output & Variables/basicprgm.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello World" << endl; // endl and "\n" are escape sequences
-    cout << "How are you?" << "\n";
-    cout << "I am new to programming";
-    return 0;
-}
